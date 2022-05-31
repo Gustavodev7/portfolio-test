8878f6fed53acdfda49db2e4e6e3462dcf1b1977
unknown <gudevcloud@gmail.com>
2022-05-31 20:22:07 -0300

    mensagem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Intro from './components/Intro';
+import About from './components/About'
 
 function App() {
   return (
 
     <div className="App">
-      <Intro/>      
+      <Intro nome="Gustavo"  cargo2="Estudante de React"/>  
+      <About/>    
     </div>
   );
 }
